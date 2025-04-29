7b37fe2518c8c4b9279bc02e2cc6b77921b2de22
Nicolas Gottiniaux <nicolas.gottiniaux@gmail.com>
2025-04-29 13:48:51 +0200

    creating card component + adding it to homepage

---
+++ my-react-app/src/components/card/card.jsx
@@ -0,0 +1,12 @@
+import "./card.scss"
+
+function Card({ title, picture }) {
+    return (
+      <div className="card">
+        <img className="card__img" src={picture} alt="" />
+        <h2 className="card__title"> {title} </h2>
+      </div>
+    )
+  }
+  
+export default Card
--- my-react-app/src/pages/home.jsx
+++ my-react-app/src/pages/home.jsx
@@ -2,6 +2,11 @@ import Header from "../components/header/header"
 import Banner from "../components/banner/banner"
 import BannerImage from '../assets/banner.jpg'
 import Footer from '../components/footer/footer'
+import Card from '../components/card/card'
+import { Link } from 'react-router'
+import AppartmentData from '../assets/logements.json'
+import './home.scss'
+import "../style/global.scss"
 
 function HomePage() {
 
@@ -13,6 +18,15 @@ function HomePage() {
         title="Chez vous, partout et ailleurs"
         className="banner"
       />
+      <section className="appartmentRent">
+        <div className="appartmentRent__card">
+          {AppartmentData.map((appartmentCard) => (
+            <Link to={`/logement/${appartmentCard.id}`} key={appartmentCard.id}>
+              <Card picture={appartmentCard.cover} title={appartmentCard.title} />
+            </Link>
+          ))}
+        </div>
+      </section>
       <Footer></Footer>
     </div>
   )
