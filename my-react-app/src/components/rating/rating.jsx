6bd9c0f77d2065c3cfed03e1e564fda3ed5dbf54
Nicolas Gottiniaux <nicolas.gottiniaux@gmail.com>
2025-05-07 09:17:11 +0200

    adding rating + tags component

---
+++ my-react-app/src/components/rating/rating.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import greyStar from '../../assets/grey_star.svg'
+import orangeStar from '../../assets/orange_star.svg'
+import "../rating/rating"
+import '../../style/global.scss'
+
+function Rating({ rating }) {
+  const range = [1, 2, 3, 4, 5]
+  return (
+    <div className="rating-container">
+      {range.map((rangeElem) =>
+        rating >= rangeElem ? (
+          <img
+            className="rating-container__stars"
+            src={orangeStar}
+            alt=""
+            key={rangeElem.toString()}
+          />
+        ) : (
+          <img
+            className="rating-container__stars"
+            src={greyStar}
+            alt=""
+            key={rangeElem.toString()}
+          />
+        )
+      )}
+    </div>
+  )
+}
+
+export default Rating
---
+++ my-react-app/src/components/tags/tags.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../tags/tags"
+import '../../style/global.scss'
+
+function Tags({ tag }) {
+  return (
+    <div>
+      <ul className="tags">
+        {tag.map((tag, index) => (
+          <li className="tags__text" key={index}>
+            {tag}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Tags
